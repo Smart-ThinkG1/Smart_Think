cc45c8172a66fe4a090152a27d259884fd1dc36d
Fernando-Henrique-SpTech <fernando.soares@sptech.school>
2024-10-21 12:44:46 -0300

    feat - trazendo algumas features novas da dash central, cards e graficos

--- public/js/graficoBodyDash.js
+++ public/js/graficoBodyDash.js
@@ -32,3 +32,64 @@ new Chart(ctx, {
         }
     }
 });
+
+const ctx2 = document.getElementById('myBarChart').getContext('2d');
+new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['Negativos', 'Positivos', 'Neutros'],
+        datasets: [{
+            axis: 'y',
+            label: 'Quantidade',
+            data: [2000, 4000, 2000], // Substitua esses valores pelos seus dados reais
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(255, 206, 86, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis: 'y',
+        responsive: true,
+        scales: {
+            x: {
+                beginAtZero: true,
+                ticks: {
+                    font: {
+                        family: 'Lato', // Define a fonte
+                        size: 10
+                    }
+                }
+            },
+            y: {
+                ticks: {
+                    font: {
+                        family: 'Lato', // Define a fonte
+                        size: 10 
+                    }
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                align: 'start',
+                text: 'Distribuição de Sentimentos',
+                font: {
+                    family: 'Lato', // Define a fonte
+                    size: 12 
+                }
+            }
+        }
+    }
+});

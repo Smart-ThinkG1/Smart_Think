29f1f4d45c4ce9e29ea0a78fb831b3f14dddde12
Fernando-Henrique-SpTech <fernando.soares@sptech.school>
2024-10-23 11:54:52 -0300

    feat - terminei o grafico de comparações e implementei o carroseu

--- public/js/graficoBodyDash.js
+++ public/js/graficoBodyDash.js
@@ -33,7 +33,9 @@ new Chart(ctx, {
     }
 });
 
-const ctx2 = document.getElementById('myBarChart').getContext('2d');
+
+// Gráfico Barra Horizontal Unidade 1 (Matriz)
+const ctx2 = document.getElementById('myBarChart1').getContext('2d');
 new Chart(ctx2, {
     type: 'bar',
     data: {
@@ -41,7 +43,7 @@ new Chart(ctx2, {
         datasets: [{
             axis: 'y',
             label: 'Quantidade',
-            data: [2000, 4000, 2000], // Substitua esses valores pelos seus dados reais
+            data: [1500, 3500, 1000], // Dados alterados
             backgroundColor: [
                 'rgba(255, 99, 132, 0.8)',
                 'rgba(75, 192, 192, 0.8)',
@@ -63,7 +65,7 @@ new Chart(ctx2, {
                 beginAtZero: true,
                 ticks: {
                     font: {
-                        family: 'Lato', // Define a fonte
+                        family: 'Lato',
                         size: 10
                     }
                 }
@@ -71,7 +73,7 @@ new Chart(ctx2, {
             y: {
                 ticks: {
                     font: {
-                        family: 'Lato', // Define a fonte
+                        family: 'Lato',
                         size: 10 
                     }
                 }
@@ -86,7 +88,129 @@ new Chart(ctx2, {
                 align: 'start',
                 text: 'Distribuição de Sentimentos',
                 font: {
-                    family: 'Lato', // Define a fonte
+                    family: 'Lato',
+                    size: 12 
+                }
+            }
+        }
+    }
+});
+
+const ctx3 = document.getElementById('myBarChart2').getContext('2d');
+new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: ['Negativos', 'Positivos', 'Neutros'],
+        datasets: [{
+            axis: 'y',
+            label: 'Quantidade',
+            data: [2500, 3000, 2500], // Dados alterados
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(255, 206, 86, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis: 'y',
+        responsive: true,
+        scales: {
+            x: {
+                beginAtZero: true,
+                ticks: {
+                    font: {
+                        family: 'Lato',
+                        size: 10
+                    }
+                }
+            },
+            y: {
+                ticks: {
+                    font: {
+                        family: 'Lato',
+                        size: 10 
+                    }
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                align: 'start',
+                text: 'Distribuição de Sentimentos',
+                font: {
+                    family: 'Lato',
+                    size: 12 
+                }
+            }
+        }
+    }
+});
+
+const ctx4 = document.getElementById('myBarChart3').getContext('2d');
+new Chart(ctx4, {
+    type: 'bar',
+    data: {
+        labels: ['Negativos', 'Positivos', 'Neutros'],
+        datasets: [{
+            axis: 'y',
+            label: 'Quantidade',
+            data: [1000, 5000, 1500], // Dados alterados
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(255, 206, 86, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis: 'y',
+        responsive: true,
+        scales: {
+            x: {
+                beginAtZero: true,
+                ticks: {
+                    font: {
+                        family: 'Lato',
+                        size: 10
+                    }
+                }
+            },
+            y: {
+                ticks: {
+                    font: {
+                        family: 'Lato',
+                        size: 10 
+                    }
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                align: 'start',
+                text: 'Distribuição de Sentimentos',
+                font: {
+                    family: 'Lato',
                     size: 12 
                 }
             }
